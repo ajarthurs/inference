9fdd86cf12c0dab3e041f96a5b2f125918d2eeea
Aaron J Arthurs <aajarthurs@gmail.com>
2019-07-18 10:26:44 -0500

    Generate quantized model from TFLite backend

--- v0.5/classification_and_detection/python/backend_tflite.py
+++ v0.5/classification_and_detection/python/backend_tflite.py
@@ -8,6 +8,8 @@ from threading import Lock
 
 import tensorflow as tf
 from tensorflow.lite.python import interpreter as interpreter_wrapper
+from tensorflow.lite.python.optimize import calibrator
+from tensorflow.python.framework.dtypes import qint8 # quantized signed 8-bit integer
 
 import backend
 
@@ -29,6 +31,8 @@ class BackendTflite(backend.Backend):
         return "NHWC"
 
     def load(self, model_path, inputs=None, outputs=None):
+        with open(model_path, 'rb') as f:
+            self.quantizer = calibrator.Calibrator(f.read())
         self.sess = interpreter_wrapper.Interpreter(model_path=model_path)
         self.sess.allocate_tensors()
         # keep input/output name to index mapping
@@ -40,7 +44,22 @@ class BackendTflite(backend.Backend):
         return self
 
     def predict(self, feed):
+        def _input_gen():
+            """Yield one batch of feed items.
+                Returns:
+                  [v] generator: A batch of N feed items.
+            """
+            for item in feed['input']:
+                yield [[item]]
+
         self.lock.acquire()
+        # generate quantized model
+        self.quantized_model = self.quantizer.calibrate_and_quantize(
+            _input_gen,
+            allow_float=False,
+            input_type=qint8,
+            output_type=qint8,
+        )
         # set inputs
         for k, v in self.input2index.items():
             self.sess.set_tensor(v, feed[k])
